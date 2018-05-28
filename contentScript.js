fcd1fe2d527eccfe4134856bd361b5250b1cb973
Karan Nehra <karanehra@gmail.com>
2018-05-29 00:52:42 +0530

    Close and back button

--- contentScript.js
+++ contentScript.js
@@ -6,12 +6,27 @@ var callback = function () {
     var dislikes = a[1].innerHTML;
 
     setTimeout(function () {
-        header_.innerHTML += '<div id="main" style="position: absolute;top: 0;left: 0;width: 100%;height: 150px;display: flex;justify-content: center"><div style="height: inherit;width: 500px;background-color: lightgray;text-align: center"><h3>Continue?</h3><h4> Likes</h4><h4> Dislikes</h4><div id="close">Close</div></div></div>';
+        // fetch('file:///home/karan/Desktop/yt-ext/background.js').then(function(data){
+        //     console.log(data)
+        // })
+        console.log(chrome.extension.getURL('some_folder/some_file.js'))
+        header_.innerHTML += '<div id="main" style="position: absolute;top: 0;left: 0;width: 100%;height: 150px;display: flex;justify-content: center">\
+        <div style="height: inherit;width: 500px;background-color: lightgray;text-align: center"> \
+            <h2>Continue?</h2> \
+            <h3>' + likes + ' Likes</h3><h4>'+dislikes+' Dislikes</h4>\
+            <div id="close">Close</div><br>\
+            <div id="back">Back</div>\
+        </div>\
+    </div>';
         $('video.html5-main-video').click();
         $('#close').click(function(){
             $('video.html5-main-video').click();
             $('#main').hide();
         });
+        $('#back').click(function(){
+            window.history.back()
+            $('#main').hide();
+        })
     }, 3000);
 
 }
@@ -24,3 +39,5 @@ if (
 } else {
     document.addEventListener("DOMContentLoaded", callback);
 }
+
+
